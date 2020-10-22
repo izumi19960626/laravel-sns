2629a046676065e25947d6fc1657d78c875bcce6
和泉直樹 <izumi@izuminaokinoMacBook-Air.local>
2020-10-22 22:14:28 +0900

    first commit

---
+++ laravel/resources/views/users/show.blade.php
@@ -0,0 +1,26 @@
+@extends('app')
+
+@section('title', $user->name)
+
+@section('content')
+  @include('nav')
+  <div class="container">
+  @include('users.user')
+   @include('users.tabs',['hasArticles'=>true,'hasLikes'=>false])
+    <!-- <ul class="nav nav-tabs nav-justified mt-3">
+      <li class="nav-item">
+        <a class="nav-link text-muted active" href="{{ route('users.show',['name'=>$user->name]) }}">
+          記事
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-muted" href="{{ route('users.likes',['name'=>$user->name]) }}">
+          いいね
+        </a>
+      </li>
+    </ul> -->
+    @foreach($articles as $article)
+     @include('articles.card')
+    @endforeach
+  </div>
+@endsection
